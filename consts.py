ac58864cc6c97476b3fc2b1a8a03b4933a002ef7
Alex S.C. <ale-xander@live.it>
2024-07-08 17:41:32 +0200

    Added some new functions

--- consts.py
+++ consts.py
@@ -12,3 +12,26 @@ ENROLLED_TEMPLATES = 3
 
 # Number of probes for each probe
 PROBES_ATTEMPTS = 3
+
+# The path to the Gallery directory
+GALLERY_PATH = "./Eval_Dataset/gallery"
+
+# The path to the Probe directory
+PROBES_PATH = "./Eval_Dataset/probes"
+
+# Name of the distance matrix
+DISTANCE_MATRIX_NAME = "distance_matrix.csv"
+
+# Threshold constants
+MIN_THRESHOLD = 0.0
+MAX_THRESHOLD = 1.0
+THRESHOLD_STEP = 0.01
+
+# Focused threshold
+#MIN_THRESHOLD = 0.5
+#MAX_THRESHOLD = 0.6
+#THRESHOLD_STEP = 0.001
+
+# Name of the evaluation metric
+EVALUATION_METRICS_NAME = "evaluation_metrics.json"
+
